c53b0f32884168c1343dabf12fa0ad6573b53340
Kseniia <kvxoxlova2005@mail.ru>
2023-03-20 19:33:51 +0300

    New lesson

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
+from DBcm import UseDatabase
+
 app = Flask(__name__)
+
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+                            'user': 'root',
+                            'password': 'Pa$$w0rd',
+                            'database': 'vsearchlogDB', }
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                    (phrase, letters, ip, browser_string, results)
+                    values
+                    (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            str(req.user_agent),
+                            res, ))
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -25,10 +41,19 @@ def entry_page() -> 'html':
                            the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
-    with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+def view_the_log() -> 'html':
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 if __name__ == '__main__':
     app.run(debug=True)
