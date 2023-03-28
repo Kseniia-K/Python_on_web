e00035a50dc0b9b853007786bb23f17406b9292c
Kseniia <kvxoxlova2005@mail.ru>
2023-03-28 12:50:13 +0300

    last flask lesson

--- webapp/checker.py
+++ webapp/checker.py
@@ -1,6 +1,10 @@
+from flask import session
+
+from functools import wraps
 def check_logged_in(func):
-    def wrapper():
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         if 'logged_in' in session:
-            return func()
+            return func(*args, **kwargs)
         return 'You are NOT logged in.'
     return wrapper
--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
 
 from DBcm import UseDatabase
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ app.config['dbconfig'] = { 'host': '127.0.0.1',
                             'user': 'root',
                             'password': 'Pa$$w0rd',
                             'database': 'vsearchlogDB', }
+
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in.'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out.'
+
 def log_request(req: 'flask_request', res: str) -> None:
 
     with UseDatabase(app.config['dbconfig']) as cursor:
@@ -41,6 +53,7 @@ def entry_page() -> 'html':
                            the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
 
     with UseDatabase(app.config['dbconfig']) as cursor:
@@ -54,6 +67,8 @@ def view_the_log() -> 'html':
                            the_title='View Log',
                            the_row_titles=titles,
                            the_data=contents,)
+
+app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 if __name__ == '__main__':
     app.run(debug=True)
